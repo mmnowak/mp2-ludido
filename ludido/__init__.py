fddae2bef0d0b17dcda17f9bb2f2813fb839be8e
Martyna Nowak <m.m.nowak187@gmail.com>
2023-10-22 18:11:54 +0000

    create activities.html and index.html templates

--- ludido/__init__.py
+++ ludido/__init__.py
@@ -10,6 +10,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
 
-from taskmanager import routes
+from ludido import routes
 
 
--- ludido/routes.py
+++ ludido/routes.py
@@ -4,3 +4,11 @@ from ludido import app, db
 @app.route("/")
 def home():
     return render_template("base.html")
+
+@app.route("/activities")
+def activities():
+    return render_template("activities.html")
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
